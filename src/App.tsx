0e2be52414481d2a7716638e45c860c527e845f9
Piotr Tarach <malfiraggraclan@gmail.com>
2026-03-14 17:56:11 +0100

    refactor: wire router and navbar to file-based feature modules
    Update App.tsx routes to use new page wrappers (BlogIndexPage,
    BlogSlugPage, ProjectsPage). Change /blog/:id to /blog/:slug. Remove
    auth/admin routes. Strip Supabase auth state and admin UI from Navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Index from "./pages/Index";
-import Projects from "./pages/Projects";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
+import ProjectsPage from "./pages/ProjectsPage";
+import BlogIndexPage from "./pages/BlogIndexPage";
+import BlogSlugPage from "./pages/BlogSlugPage";
 import SkillsPage from "./pages/SkillsPage";
 import HowIDoItIndexPage from "./pages/HowIDoItIndexPage";
 import HowIDoItSlugPage from "./pages/HowIDoItSlugPage";
-import Auth from "./pages/Auth";
-import AdminProject from "./pages/AdminProject";
-import AdminBlog from "./pages/AdminBlog";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -27,15 +24,12 @@ const App = () => (
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/skills" element={<SkillsPage />} />
           <Route path="/how-i-do-it" element={<HowIDoItIndexPage />} />
           <Route path="/how-i-do-it/:slug" element={<HowIDoItSlugPage />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/admin/project" element={<AdminProject />} />
-          <Route path="/admin/blog" element={<AdminBlog />} />
+          <Route path="/blog" element={<BlogIndexPage />} />
+          <Route path="/blog/:slug" element={<BlogSlugPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
