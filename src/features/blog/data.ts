cfc9bf8d9ee46da4f170409ce28e7ac39cc80b0f
Piotr Tarach <malfiraggraclan@gmail.com>
2026-03-15 19:59:06 +0100

    feat(blog): add category field and BlogOutletContext type

--- src/features/blog/data.ts
+++ src/features/blog/data.ts
@@ -3,7 +3,14 @@ export interface BlogPost {
   title: string;
   date: string;
   tags: string[];
+  category: string;
   excerpt: string;
+}
+
+export interface BlogOutletContext {
+  filteredPosts: BlogPost[];
+  activeTags: string[];
+  allTags: string[];
 }
 
 export const blogPosts: BlogPost[] = [];
